da51744522d33d7fe8958973f9b60e43ae746e9f
Luke Fleming <lukefleming@Lukes-MBP.fios-router.home>
2022-05-05 19:01:31 -0400

    Project Clean Up

--- server.js
+++ server.js
@@ -99,37 +99,3 @@ const port = process.env.PORT || 3001;
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)
 });
-
-// require('dotenv').config();
-// const express = require('express');
-// const path = require('path');
-// const favicon = require('serve-favicon');
-// const logger = require('morgan');
-// const PORT = process.env.PORT || 3001;
-
-// // Mongoose Connection has access to config -> database.js via dotenv
-// require('./config/database.js');
-
-// const app = express();
-
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(favicon(path.join(__dirname,'build','favicon.ico')));
-// app.use(express.static(path.join(__dirname,'build')));
-
-// // path.join allows routes to work on any computer type windows, mac, linux, ...etc
-
-// // API
-
-// app.use('/api/users', require('./routes/api/users'))
-
-// // Catch All API
-
-// app.get('/*', (req,res) => {
-//     res.sendFile(path.join(__dirname,'build','index.html'))
-// })
-
-// app.listen(PORT, () => {
-//     console.log(`Backend is listening on ${PORT}`)
-// })
